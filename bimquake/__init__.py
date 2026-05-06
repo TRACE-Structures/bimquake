3b0bed6948eb34c5451da2525c5f1cc53f7d3b38
Bence Popovics <47786942+popbence@users.noreply.github.com>
2026-05-06 20:00:26 +0200

    Delete bimquake directory

--- bimquake/__init__.py
+++
@@ -1,7 +0,0 @@
-__version__ = '0.1.3'
-
-from .eq_resilience_library import *
-
-
-from .ifc_processing import *
-
--- bimquake/ifc_processing/__init__.py
+++
@@ -1,7 +0,0 @@
-from .building import *
-from .IFC_objects import *
-from .simple_objects import *
-from .slab import *
-from .structural_partitions import *
-from .wall import *
-from .utils import *
